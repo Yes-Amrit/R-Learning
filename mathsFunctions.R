578fbcc954c76a7c6037512498f092cd43b91385
Amrit Raj <143371086+Yes-Amrit@users.noreply.github.com>
2025-06-27 17:30:48 +0530

    Create mathsFunctions.R

---
+++ mathsFunctions.R
@@ -0,0 +1,31 @@
+> > # simple math in R
+          > 10+5
+        [1] 15
+        > 10-5
+        [1] 5
+        > 10/5
+        [1] 2
+        > 10*5
+        [1] 50
+        > # built in math function: min() and max()
+          > max(5,10,15)
+        [1] 15
+        > min(5,10,15)
+        [1] 5
+        > # the sqrt() function returns the square root of a number:
+          > sqrt(16)
+        [1] 4
+        > sqrt(64)
+        [1] 8
+        > # the abs() fucntion retuns the absolute(positive) value of a number
+          > abs(-4.5)
+        [1] 4.5
+        > # ceiling() and floor(): the ceiling() function rounds a number upwards to its nearest integer, and the floor() function round a number downwards to its nearest intger and return the result
+          > ceiling(1.4)
+        [1] 2
+        > ceiling(1.2)
+        [1] 2
+        > floor(1.9)
+        [1] 1
+        > floor(1.5)
+        [1] 1 
